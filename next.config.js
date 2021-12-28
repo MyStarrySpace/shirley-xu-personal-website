d8a6803ffbf66a10b65b244c86ef33d24c83217e
Shirley Xu <shirleyxu@umass.edu>
2021-12-28 13:31:35 -0500

    Reset next.config.js, cleaned packages and remade package lock

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-// next.config.js
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-
 /** @type {import('next').NextConfig} */
-module.exports = withPlugins([
-  [optimizedImages, {
-    /* config for next-optimized-images */
-  }],
-
-]);
+module.exports = {
+  reactStrictMode: true,
+}
