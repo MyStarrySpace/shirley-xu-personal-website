a91b352079acc04a172cd5745e3578842d38d52d
MyStarrySpace <Mirative@gmail.com>
2021-12-29 00:50:08 -0500

    Blarg
    Blarg

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -19,7 +19,9 @@ type ResponseData = {
 export default async (req:NextApiRequest, res:NextApiResponse<ResponseData>) => {
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_ADDRESS,
       pass: process.env.EMAIL_PASSWORD,
