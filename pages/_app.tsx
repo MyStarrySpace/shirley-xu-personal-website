215ee988b4abf48a20ae094cf2fedd042b90cd8d
MyStarrySpace <Mirative@gmail.com>
2021-12-29 18:31:54 -0500

    Bugfixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ import styles from '../styles/Home.module.css'
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <>
-      <Head data={pageProps.header}>
+      <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@900&family=Montserrat&family=Telex&display=swap" rel="stylesheet" />
