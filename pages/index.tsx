edfb76651940529277e43a1d50d283b8c1f1de78
MyStarrySpace <Mirative@gmail.com>
2021-12-29 18:31:54 -0500

    Bugfixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import ProjectList from './components/projectlist'
+import ProjectList from './/components/projectlist'
 
 import Container from 'react-bootstrap/Container'
 import Link from 'next/link'
