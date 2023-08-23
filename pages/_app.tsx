f32f09cd5b336560d4e8934cf7818dfcbfeda73e
MyStarrySpace <25332257+MyStarrySpace@users.noreply.github.com>
2023-08-23 13:47:11 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@900&family=Montserrat&family=Telex&display=swap" rel="stylesheet" />
       </Head>
 
