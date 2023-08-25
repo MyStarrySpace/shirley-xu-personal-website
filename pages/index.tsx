a59711b8819ba59cc38a111b3b52537f98d22de2
MyStarrySpace <25332257+MyStarrySpace@users.noreply.github.com>
2023-08-25 03:30:58 -0400

    Fixed the centering of layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,51 +21,50 @@ const Intro: NextPage = () => {
     <div className={styles.introbg}>
       <div className="container-fluid">
         <div className="row justify-content-md-center">
-          <div className={`col-sm-12 col-lg-8 ${styles.center}`}>  
-            <motion.div initial="initial" animate="animate" transition={{ delay: 0.2 }} variants={{ 
-                initial: { opacity: 0, y: -300}, 
-                animate: { opacity: 1, y: 0}, 
-              }}>
+          <div className={`col-sm-12 col-lg-8 ${styles.center}`}>
+            <motion.div initial="initial" animate="animate" transition={{ delay: 0.2 }} variants={{
+              initial: { opacity: 0, y: -300 },
+              animate: { opacity: 1, y: 0 },
+            }}>
               <div className={`${styles.focusbox} col border rounded mb-4`}>
                 <div className={`p-5 ${styles.primary}`}>
-                  <div className={`row ${styles.introsection}`}>
-                    <div className="col-2"></div>
+                  <div className={`row ${styles.introsection} ${styles.centerColumns}`}>
                     <div className={`col-4 ${styles.introtitle}`}>
                       Hello! My name is
                       <h1 className="my-0">Shirley Xu</h1>
                     </div>
                     <div className={["col-3", styles.portraitdiv].join(' ')}>
-                      <Image src={Portrait} alt={"Portrait of Shirley Xu"} className={styles.portrait} width="150" height="150"/>
+                      <Image src={Portrait} alt={"Portrait of Shirley Xu"} className={styles.portrait} width="150" height="150" />
                     </div>
-                    <div className="col-3"></div>
                   </div>
+
                 </div>
                 <div className="bg-light p-5">
                   <p className="my-0 pb-4">
-                  I am Shirley Xu, a software engineer, bioinformatician, and self-taught artist with a passion for blending creativity, technology, and design. 
-                  Whether I&apos;m designing characters for gaming projects, analyzing data with bioinformatics tools or building responsive web applications, I approach every challenge with adaptability, innovation, and a commitment to excellence. 
-                  My diverse educational background, hands-on experience, and artistic abilities fuel my desire to create meaningful and well-designed products that resonate with users.
+                    I am Shirley Xu, a software engineer, bioinformatician, and self-taught artist with a passion for blending creativity, technology, and design.
+                    Whether I&apos;m designing characters for gaming projects, analyzing data with bioinformatics tools or building responsive web applications, I approach every challenge with adaptability, innovation, and a commitment to excellence.
+                    My diverse educational background, hands-on experience, and artistic abilities fuel my desire to create meaningful and well-designed products that resonate with users.
                   </p>
-                  <motion.div className="gx-5" initial="initial" animate="animate" transition={{ delay: 0.5 }} variants={{ 
-                    initial: { opacity: 0}, 
-                    animate: { opacity: 1}, 
+                  <motion.div className="gx-5" initial="initial" animate="animate" transition={{ delay: 0.5 }} variants={{
+                    initial: { opacity: 0 },
+                    animate: { opacity: 1 },
                   }}>
                     <div className="row">
                       <div className="col-sm-0 col-lg-3"></div>
                       <div className="col-sm-6 col-lg-3">
                         <Link href="/aboutme">
                           <button className={`btn p-2 ${styles.aboutmebtn}`}>
-                              <a className={`link ${styles.link}`}>About Me</a>
+                            <a className={`link ${styles.link}`}>About Me</a>
                           </button>
-                        </Link> 
+                        </Link>
                       </div>
                       <div className="col-sm-6 col-lg-3">
                         <Link href="/contact">
                           <button className={`btn p-2 ${styles.contactmebtn}`}>
-                              <a className={`link ${styles.link}`}>Contact Me</a>
+                            <a className={`link ${styles.link}`}>Contact Me</a>
                           </button>
                         </Link>
-                      </div> 
+                      </div>
                       <div className="col-sm-0 col-lg-3"></div>
                     </div>
                   </motion.div>
@@ -89,20 +88,26 @@ interface CardContents {
 
 const Services: NextPage = () => {
 
-  let contents0: CardContents = {title: "UI Design and Development", 
+  let contents0: CardContents = {
+    title: "UI Design and Development",
     body: "Are you looking for someone to help you design and implement your user interfaces? You've come to the right place! \
     Having studied user experience, data visualization, and human-computer interaction, \
-    I am experienced with creating delightful visual and functional experiences for users of all kinds.", image: imgWebDesign, index: 0, delay: 0}
-  let contents1: CardContents = {title: "Full-stack Development", 
+    I am experienced with creating delightful visual and functional experiences for users of all kinds.", image: imgWebDesign, index: 0, delay: 0
+  }
+  let contents1: CardContents = {
+    title: "Full-stack Development",
     body: "Having graduated with a B.S. in Computer Science from the University of Massachusetts Amherst, \
     I have worked with both frontend and backend programming, including networking, design, and full-stack development. \
-    I have vast experience with the MERN stack, Typescript, and APIs, which I have also used in my personal website.", image: imgWebDevelopment, index: 1, delay: 0.2}
-  let contents2: CardContents = {title: "Illustration", 
+    I have vast experience with the MERN stack, Typescript, and APIs, which I have also used in my personal website.", image: imgWebDevelopment, index: 1, delay: 0.2
+  }
+  let contents2: CardContents = {
+    title: "Illustration",
     body: "I have been drawing for 7+ years and won the Gold Scholastic Art Award in 2016. I have a passion for art and illustration; in fact, \
     I was accepted into the Rhode Island School of Design, one of the most prestigious art schools. \
-    Even though I chose a different path, I still create digital art and have done various concept art pieces as well.", image: imgIllustration, index: 2, delay: 0.4}
+    Even though I chose a different path, I still create digital art and have done various concept art pieces as well.", image: imgIllustration, index: 2, delay: 0.4
+  }
   return (
-    <div className={styles.purplebg}> 
+    <div className={styles.purplebg}>
       <div className={["container-fluid p-5"].join(' ')}>
         <div className={[styles.title, "py-4"].join(' ')}>
           <h2> What can I do for you? </h2>
@@ -139,7 +144,7 @@ const Projects: NextPage = () => {
       </div>
     </div>
   )
-  
+
 }
 
 const Home: NextPage = () => {
@@ -147,7 +152,7 @@ const Home: NextPage = () => {
     <>
       <Intro />
       <Services />
-      <Projects />        
+      <Projects />
     </>
   );
 }
